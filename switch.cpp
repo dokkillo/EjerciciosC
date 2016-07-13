74b96347c65696c12f027c4d760966d401504731
dokkillo <devgcosta@gmail.com>
2016-07-13 21:55:28 +0200

    add switch test

---
+++ switch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x=0;
+  cin>>x;
+  switch(x)
+    {
+    case 0:
+      cout << "es 0";
+    break;
+    default:
+      cout  << x;
+    }
+  return 0;
+
+}
